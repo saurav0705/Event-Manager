c0068863c8894d0729c791a4008b1219b0b97883
saurav0705 <sauravaggarwal98@gmail.com>
2020-05-22 02:09:38 +0530

    added animation

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,14 +1,15 @@
-import React,{useEffect} from 'react';
+import React,{useEffect, useRef} from 'react';
 import './Banner.scss';
 import {IoMdPerson} from 'react-icons/io';
 import {AiOutlineQrcode} from 'react-icons/ai';
 import {TimelineLite,Power2} from 'gsap';
 const Banner = () => {
+    let references = useRef({headline:"",card1:"",card2:""});
     let timeline = new TimelineLite();
     useEffect(()=>{
-        timeline.from(document.querySelector('#headline'),1,{y:-100,opacity:0,ease:Power2.easeInOut})
-                .from(document.querySelector('#card-1'),1,{x:-100,opacity:0,ease:Power2.easeInOut})
-                .from(document.querySelector('#card-2'),1,{rotate:0,x:-100,opacity:0,ease:Power2.easeInOut})
+        timeline.from(references.current.headline,1,{y:-100,opacity:0,ease:Power2.easeInOut})
+                .from(references.current.card1,1,{x:-100,opacity:0,ease:Power2.easeInOut})
+                .from(references.current.card2,1,{rotate:0,x:-100,opacity:0,ease:Power2.easeInOut})
                 
         
 
@@ -20,18 +21,18 @@ const Banner = () => {
     ]
     return (
         <div className="banner" id="banner">
-            <div className="headline" id="headline">
+            <div className="headline" id="headline" ref = {(el)=>{references.current.headline = el}}>
             {bannerHeadLines.map((headline,index) => (<div className="text" key={"banner"+index}>{headline}</div>))}
             
             </div>
             <div className="image">
-                <div className="card card-1" id="card-1">
+                <div className="card card-1" ref = {(el)=>{references.current.card1 = el}}>
                     <div className="title">Event</div>
                     <div className="icon"><IoMdPerson/></div>
                     <div className="qr"><AiOutlineQrcode/></div>
                     <div className="name">Jane Doe</div>
                 </div>
-                <div className="card card-2" id="card-2">
+                <div className="card card-2" ref = {(el)=>{references.current.card2 = el}}>
                 <div className="title">Event</div>
                     <div className="icon"><IoMdPerson/></div>
                     <div className="qr"><AiOutlineQrcode/></div>
