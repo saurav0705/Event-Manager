67881fa85c6929759aaed107b0fd9e066115b528
saurav0705 <sauravaggarwal98@gmail.com>
2020-06-06 14:54:29 +0530

    added social url

--- src/components/Home/Contact/Social/Social.js
+++ src/components/Home/Contact/Social/Social.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {FaGithubSquare,FaPhone,FaFacebookSquare,FaLinkedin,FaSnapchatSquare,FaInstagram,FaTwitterSquare} from 'react-icons/fa';
 import {MdEmail} from 'react-icons/md';
 const Social = () => {
+    
     const icon = (key) => {
         switch(key.toUpperCase()){
             case 'EMAIL' : return <MdEmail/>
@@ -17,20 +18,20 @@ const Social = () => {
 
     }
     const social = {
-        email:"",
-        facebook:"",
-        twitter:"",
-        snapchat:"",
-        instagram:"",
-        linkedin:"",
-
+        email:"https://www.gmail.com",
+        facebook:"https://www.facebook.com",
+        twitter:"https://www.twitter.com",
+        snapchat:"https://www.snapchat.com",
+        instagram:"https://www.instagram.com",
+        linkedin:"https://www.linkedin.com",
     }
+
     return (
         <div className="social">
                     <div className="heading">Social</div>
                     <div className="tile-container">
-                {Object.keys(social).map((social,index) => {
-                    return(<div  className="tile" key={social} onClick={()=>window.open(social === 'email' ? "mailto:"+social[social]:social[social])}>{icon(social)}</div>)
+                {Object.keys(social).map((soc,index) => {
+                    return(<div  className="tile" key={soc} onClick={()=>window.open(social[soc])} >{icon(soc)}</div>)
                 })}
                 </div>
                 </div>
